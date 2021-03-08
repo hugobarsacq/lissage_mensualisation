7e8dda1ade54b58c53350fc1775086fe39246477
Hugo Barsacq <58510166+hugobarsacq@users.noreply.github.com>
2021-03-08 18:48:15 +0100

    Delete lissage_mensualisation-master directory

--- lissage_mensualisation-master/models/res_config_settings.py
+++
@@ -1,9 +0,0 @@
-# from odoo import api, fields, models
-#
-#
-# class ResConfigSettings(models.TransientModel):
-#     _inherit = 'res.config.settings'
-#
-#     journal_hugo = fields.Many2one('account.journal', string="Journal")
-#
-#
